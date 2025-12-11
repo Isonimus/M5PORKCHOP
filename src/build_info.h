53694ad59fe0b3b1f476b673857bbf2f21148d0b
neledov <neledov@gmail.com>
2025-12-11 09:32:06 +0100

    chore: remove dead code from avatar.h
    - Remove flashState() declaration (never implemented)
    - Remove flashCyclesRemaining, flashStateType, returnStateType, lastFlashCycleTime static members (never defined)
    - Remove 8 stale extern AVATAR_* declarations (arrays use _R/_L suffixes)

--- src/build_info.h
+++ src/build_info.h
@@ -1,4 +1,4 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2025-12-11T04:51:20.160130"
+#define BUILD_TIME "2025-12-11T09:30:09.172653"
 #define BUILD_VERSION "0.1.0"
--- src/piglet/avatar.h
+++ src/piglet/avatar.h
@@ -23,9 +23,6 @@ public:
     static void blink();
     static void wiggleEars();
     
-    // Flash state temporarily (e.g., EXCITED on handshake capture)
-    static void flashState(AvatarState state, uint8_t cycles);
-    
     // Grass animation control
     static void setGrassMoving(bool moving);
     static bool isGrassMoving() { return grassMoving; }
@@ -40,12 +37,6 @@ private:
     static uint32_t lastBlinkTime;
     static uint32_t blinkInterval;
     
-    // Flash state system (temporary state override)
-    static uint8_t flashCyclesRemaining;
-    static AvatarState flashStateType;
-    static AvatarState returnStateType;
-    static uint32_t lastFlashCycleTime;
-    
     // Grass animation state
     static bool grassMoving;
     static uint32_t lastGrassUpdate;
@@ -56,13 +47,3 @@ private:
     static void drawGrass(M5Canvas& canvas);
     static void updateGrass();
 };
-
-// Avatar frames - 5 lines each
-extern const char* AVATAR_NEUTRAL[];
-extern const char* AVATAR_HAPPY[];
-extern const char* AVATAR_EXCITED[];
-extern const char* AVATAR_HUNTING[];
-extern const char* AVATAR_SLEEPY[];
-extern const char* AVATAR_SAD[];
-extern const char* AVATAR_ANGRY[];
-extern const char* AVATAR_BLINK[];
