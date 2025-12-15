d2d82af526b0aff6f3e126ea34a346ba40d39dc9
neledov <neledov@gmail.com>
2025-12-15 21:47:25 +0100

    fix: bump build_info.h to v0.1.4

--- src/build_info.h
+++ src/build_info.h
@@ -1,4 +1,4 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2025-12-15T20:52:41.262324"
-#define BUILD_VERSION "0.1.3"
+#define BUILD_TIME "2025-12-15T22:00:00.000000"
+#define BUILD_VERSION "0.1.4"
