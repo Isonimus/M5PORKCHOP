05ef1e18036180c097dce10ef0766a2ab86a5234
Iker <iker.laforga@grupogestionet.com>
2025-12-29 19:03:36 +0100

    chore: update build info

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2025-12-27T14:46:48.025685"
+#define BUILD_TIME "2025-12-29T18:41:28.163217"
 #define BUILD_VERSION "0.1.7_DNKROZ"
-#define BUILD_COMMIT "7f7ff6a"
+#define BUILD_COMMIT "0111f65"
